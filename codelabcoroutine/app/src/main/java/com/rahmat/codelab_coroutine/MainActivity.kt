639e5efad53fe1f6d953f88aa5c03cdb4fd4e29c
Muh. Rahmatullah <muhammadrahmatullah1@gmail.com>
2019-03-10 14:52:18 +0700

    first commit, init the repo

---
+++ codelabcoroutine/app/src/main/java/com/rahmat/codelab_coroutine/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rahmat.codelab_coroutine
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
